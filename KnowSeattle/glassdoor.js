68ed79c15571d911d18afad34f69c29ceae25493
Kellan Nealy <gellinn@uw.edu>
2017-01-10 22:44:45 -0800

    fixed API access for Indeed and GlassDoor:
    Fixed the node scripts to successfully access Indeed API and Glassdoor API, and added some html for an Indeed Job search widget, as well as some attempts in Python.

--- KnowSeattle/glassdoor.js
+++ KnowSeattle/glassdoor.js
@@ -1,22 +1,30 @@
-// Uses glassdoor API, getting errors
-var Glassdoor = require('machinepack-glassdoor');
+// using http module to access glassdoor API
+// docs https://www.glassdoor.com/developer/index.htm
+var http = require('http');
 
-// Get company information
-Glassdoor.getCompany({
-    partnerId: '114236',
-    partnerKey: 'j1ERnurd9SI',
-    userIp: '0.0.0.0',
-    userAgent: '',
-    q: 'Microsoft',
-    l: 'Seattle, WA, United States',
-}).exec({
-    // An unexpected error occurred.
-    error: function (err) {
-        console.log('failure');
-        console.log(err);
-    },
-    // OK.
-    success: function () {
-        console.log('success');
-    },
-});
+var options = {
+  host: 'api.glassdoor.com',
+  path: '/api/api.htm?t.p=114236&t.k=j1ERnurd9SI&userip=0.0.0.0'
+    + '&useragent=&format=json&v=1&action=jobs-stats&l=Seattle&returnEmployers=true&admLevelRequested=1'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('error', function(err) {
+      console.log('failure accessing Glassdoor API');
+      console.log(err);
+  })
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
--- KnowSeattle/indeed.js
+++ KnowSeattle/indeed.js
@@ -1,8 +1,36 @@
-var unirest = require('unirest');
-// These code snippets use an open-source library. http://unirest.io/nodejs
-unirest.get("https://indeed-indeed.p.mashape.com/apigetjobs?publisher=9876703242051712&format=json&jobkeys=<required>&v=2")
-    .header("X-Mashape-Key", "Gxi2KVML7HmshwqdoBMWQE1ilrgWp19pNfvjsnHRzVXCkjgeYC")
-    .header("Accept", "application/json")
-    .end(function (result) {
-        console.log(result.status, result.headers, result.body);
-    });
+// publisher ID for Indeed: 9876703242051712
+// using http module to access Indeed API
+// docs https://ads.indeed.com/jobroll/xmlfeed
+var http = require('http');
+
+var options = {
+  host: 'api.indeed.com',
+  path: '/ads/apisearch?publisher=9876703242051712'
+    + '&q=java'
+    + '&l=austin%2C+tx'
+    + '&sort=&radius=&st=&jt=&start=&limit='
+    + '&fromage=&filter=&latlong=1&co=us'
+    + '&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29'
+    + '&v=2'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('error', function(err) {
+      console.log('failure accessing Indeed API');
+      console.log(err);
+  })
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
