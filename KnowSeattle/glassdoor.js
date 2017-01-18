09938f0c059436c3a6f9454b119d14d94c8e60d5
Kellan Nealy <gellinn@uw.edu>
2017-01-17 17:12:05 -0800

    new features for glassdoor module:
    accepts company name in parameter, so the module can be used via command
    from the indeed module.  Also parses important company details, ratings,
    etc from the company JSON response from glassdoor's API.

--- KnowSeattle/glassdoor.js
+++ KnowSeattle/glassdoor.js
@@ -1,32 +1,58 @@
-// using http module to access glassdoor API
-// docs https://www.glassdoor.com/developer/index.htm
+/**
+* using http module to access glassdoor API
+* docs https://www.glassdoor.com/developer/index.htm
+* COMMAND LINE ARGS: "glassdoor.js -c <company_name>"
+*/
 var http = require('http');
 
+//parse command line args
+var company = "";
+if (process.argv.indexOf("-c") != -1){ //does our flag exist?
+    company = process.argv[process.argv.indexOf("-c") + 1]; //grab the next item
+} else {
+    throw new Error('No company passed to glassdoor module');
+}
+
+var encodedPath = encodeURI('/api/api.htm?'
+    + 't.p=114236&t.k=j1ERnurd9SI'
+    + '&userip=0.0.0.0&useragent=&format=json&v=1'
+    + '&action=employers'
+    + '&city=seattle&state=WA'
+    + '&q=' + company);
+
 var options = {
-  host: 'api.glassdoor.com',
-  path: '/api/api.htm?t.p=114236&t.k=j1ERnurd9SI&userip=0.0.0.0'
-    + '&useragent=&format=json&v=1&action=jobs-stats'
-    + '&l=Seattle&returnJobTitles=true&returnCities=true&returnEmployers=true'
-    + '&admLevelRequested=2'
+    host: 'api.glassdoor.com',
+    path: encodedPath
 };
 
 callback = function(response) {
-  var str = '';
+    var str = '';
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
 
-  response.on('error', function(err) {
-      console.log('failure accessing Glassdoor API');
-      console.log(err);
-  })
+    response.on('error', function(err) {
+        console.log('failure accessing Glassdoor API');
+        console.log(err);
+    })
 
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        console.log(company + " Details:")
+        var JSONObject = JSON.parse(str);
+
+        //get employers array from JSON and find best match
+        var employersArray = JSONObject.response.employers;
+        var bestMatchObj = employersArray[0]; // already sorted by best match
+
+        console.log("    Exact Match: " + bestMatchObj.exactMatch);
+        console.log("    Industry: " + bestMatchObj.industryName);
+        console.log("    Total Ratings: " + bestMatchObj.numberOfRatings);
+        console.log("    Overall Rating: " + bestMatchObj.overallRating + " "
+            + bestMatchObj.ratingDescription);
+    });
 }
 
 http.request(options, callback).end();
