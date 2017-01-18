8a1e6af517caa5988f30e163491912a907573efa
Kellan Nealy <gellinn@uw.edu>
2017-01-17 20:38:17 -0800

    New Features:
    New command line param for glassdoor.js:
    glassdoor.js -c <company_name_goes_here> -t <job_title_goes_here>
    
    This fixes the async error I was getting before, where job titles and company names weren't matching up with results from running glassdoor.js from indeed.js
    This is now fixed and console output makes sense, error handling is also better.
    
    Slight code cleanup in CrimeRadius.

--- KnowSeattle/glassdoor.js
+++ KnowSeattle/glassdoor.js
@@ -1,25 +1,30 @@
 /**
 * using http module to access glassdoor API
-* docs https://www.glassdoor.com/developer/index.htm
-* COMMAND LINE ARGS: "glassdoor.js -c <company_name>"
+* DOCS: https://www.glassdoor.com/developer/index.htm
+* COMMAND LINE EXAMPLE:
+* "glassdoor.js -c <company_name_goes_here> -t <job_title_goes_here>"
 */
 var http = require('http');
 
 //parse command line args
 var company = "";
-if (process.argv.indexOf("-c") != -1){ //does our flag exist?
-    company = process.argv[process.argv.indexOf("-c") + 1]; //grab the next item
+var title = "";
+// check if our flags exist
+if (process.argv.indexOf("-c") != -1 && process.argv.indexOf("-t") != -1) {
+    company = process.argv[process.argv.indexOf("-c") + 1];
+    title = process.argv[process.argv.indexOf("-t") + 1];
 } else {
-    throw new Error('No company passed to glassdoor module');
+    throw new Error('No company and/or title passed to glassdoor module...');
 }
 
-var encodedPath = encodeURI('/api/api.htm?'
+var path = '/api/api.htm?'
     + 't.p=114236&t.k=j1ERnurd9SI'
     + '&userip=0.0.0.0&useragent=&format=json&v=1'
     + '&action=employers'
     + '&city=seattle&state=WA'
-    + '&q=' + company);
+    + '&q=' + company;
 
+var encodedPath = encodeURI(path);
 var options = {
     host: 'api.glassdoor.com',
     path: encodedPath
@@ -40,18 +45,27 @@ callback = function(response) {
 
     //the whole response has been recieved, so we just print it out here
     response.on('end', function () {
-        console.log(company + " Details:")
+        console.log("Job Title: " + title);
+        console.log(company + " Details:");
         var JSONObject = JSON.parse(str);
 
         //get employers array from JSON and find best match
         var employersArray = JSONObject.response.employers;
         var bestMatchObj = employersArray[0]; // already sorted by best match
-
-        console.log("    Exact Match: " + bestMatchObj.exactMatch);
-        console.log("    Industry: " + bestMatchObj.industryName);
-        console.log("    Total Ratings: " + bestMatchObj.numberOfRatings);
-        console.log("    Overall Rating: " + bestMatchObj.overallRating + " "
-            + bestMatchObj.ratingDescription);
+        //try/catch for error handling
+        try {
+            console.log("    Exact Match: " + bestMatchObj.exactMatch);
+            console.log("    Industry: " + bestMatchObj.industryName);
+            console.log("    Total Ratings: " + bestMatchObj.numberOfRatings);
+            console.log("    Overall Rating: " + bestMatchObj.overallRating + " "
+                + bestMatchObj.ratingDescription);
+        } catch (err) {
+            console.log('ERROR getting company details for ' + company + ':\n');
+            console.error(err);
+            console.log('JSON blob:\n' + str + '\n'
+                + 'Query: ' + path
+                + '\n==== E N D ==== E R R O R ==========');
+        }
     });
 }
 
--- KnowSeattle/indeed.js
+++ KnowSeattle/indeed.js
@@ -1,17 +1,29 @@
-// publisher ID for Indeed: 9876703242051712
-// using http module to access Indeed API
-// docs https://ads.indeed.com/jobroll/xmlfeed
+/**
+* using http module to access indeed API
+* publisher ID for Indeed: 9876703242051712
+* DOCS: https://ads.indeed.com/jobroll/xmlfeed
+* COMMAND LINE ARGS: "indeed.js -z <zipcode_goes_here>"
+*/
 var http = require('http');
 var parseString = require('xml2js').parseString;
-var rjsonSearch = require('rjson-search');
+var commandProcess = require('child_process');
+var requiredScript = './glassdoor.js';
 
-// Zipcode is the key param for query
-var zipcode = "98105";
+//parse command line args for zipcode
+var zipcode = "";
+if (process.argv.indexOf("-z") != -1){ //does our flag exist?
+    zipcode = process.argv[process.argv.indexOf("-z") + 1]; //grab the next item
+} else {
+    throw new Error('No zipcode passed to indeed module');
+}
+
+//query for narrowing down better results
+var query = "great";
 
 var options = {
     host: 'api.indeed.com',
     path: '/ads/apisearch?publisher=9876703242051712'
-        + '&q=great'
+        + '&q=' + query
         + '&l=' + zipcode
         + '&sort=&radius=1&st=&jt=fulltime&start='
         + '&limit=1000'
@@ -43,18 +55,58 @@ callback = function(response) {
             var JSONObject = result;
             var totalRes = JSONObject.response.totalresults[0];
             console.log("Total Jobs in zipcode " + zipcode + ": " + totalRes);
+
+            // Iterate through Jobs array in returned JSON
             // Can also possibly show a map (data structure) of companies/openings
             var jobsArray = JSONObject.response.results[0].result;
             for(var i = 0; i < jobsArray.length; i++) {
-                var companyName = jobsArray[i].company[0];
                 var jobTitle = jobsArray[i].jobtitle[0];
-                console.log("Company: " + companyName);
-                console.log("Title: " + jobTitle + "\n\n");
+                var companyName = jobsArray[i].company[0];
+
+                /* one exec method for invoking glassdoor module */
+                commandProcess.exec('node ' + requiredScript
+                    + ' -c \"' + companyName + '\"'
+                    + ' -t \"' + jobTitle + '\"', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`execSync error: ${error}`);
+                        return;
+                    } else {
+                        // print stdout and stderr when this callback finishes
+                        console.log(`${stdout}`);
+                        console.log(`${stderr}`);
+                    }
+                });
             }
-            //print blob
-            console.log(JSON.stringify(jobsArray));
+            //print full blob
+            //console.log(JSON.stringify(result));
         });
     });
 }
 
 http.request(options, callback).end();
+
+/**
+* Child process code for invoking glassdoor module
+* source: http://stackoverflow.com/questions/22646996/how-do-i-run-a-node-js-script-from-within-another-node-js-script
+*/
+function runScript(scriptPath, companyOption, callback) {
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+    var process = childProcess.exec(scriptPath
+        + " -c \"" + companyOption + "\"");
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+}
