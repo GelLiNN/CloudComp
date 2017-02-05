b4617aedcb0bc275125b01f6ad904feeb7d3d5f4
Kellan Nealy <gellinn@uw.edu>
2017-02-05 06:21:41 -0800

    Delete unnecessary files:
    glassdoor.py and indeed.py are being abandoned, no use for Python here anymore

--- KnowSeattle/glassdoor.py
+++
@@ -1,9 +0,0 @@
-import urllib2, sys
-from BeautifulSoup import BeautifulSoup
-
-url = "http://api.glassdoor.com/api/api.htm?t.p=114236&t.k=j1ERnurd9SI&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q="
-hdr = {'User-Agent': ''}
-req = urllib2.Request(url,headers=hdr)
-response = urllib2.urlopen(req)
-soup = BeautifulSoup(response)
-print(response)
